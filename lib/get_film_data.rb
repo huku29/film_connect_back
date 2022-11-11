525bc122941c0f77eb3fddba0053d9c43598fdad
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-11-11 21:23:49 +0900

    Add:lib配下にファイルを作成し、自動で読み込むためのコードを記載 #5

---
+++ lib/get_film_data.rb
@@ -0,0 +1,32 @@
+require 'faraday'
+require 'faraday/net_http'
+Faraday.default_adapter = :net_http
+
+class GetFilmData
+
+  BASE_URL = "https://api.themoviedb.org/3"
+
+  def initialize
+    @api_key = ENV['VITE_TMDB_API_KEY']
+    @connection = Faraday.new(ssl: { ca_path: "/usr/local/etc/openssl@3" }, params: { api_key: @api_key, language: "ja" })
+  end
+
+  def search_films(search_word, page)
+    @connection.params[:query] = search_word
+    @connection.params[:page] = page
+    response = @connection.get("#{BASE_URL}/search/movie")
+    response.body
+  end
+
+  class << self
+    def client
+      GetFilmData.new
+    end
+
+    def search_films(search_word, page)
+      client.search_films(search_word, page)
+    end
+
+  end
+  
+end

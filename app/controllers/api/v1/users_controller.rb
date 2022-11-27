78cb6507f01364163a2b4beafcfdd412dcc1d4ca
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-11-27 11:24:06 +0900

    Add:usersコントローラーの作成 #9

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      include FirebaseAuthConcern
+      before_action :set_auth, only: %i[create update]
+
+      include CreateUserConcern
+      def create
+        create_user(@auth,params[:twitterUserName])
+      end
+
+      # include UpdateUserConcern
+      # def update
+      #   update_user(@auth)
+      # end
+
+      private
+
+      def set_auth
+        @auth = authenticate_token_by_firebase
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/users', to: 'users#create'
       get 'films/search'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

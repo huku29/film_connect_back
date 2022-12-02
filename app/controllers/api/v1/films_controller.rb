23d4e150539d696471fb54facf7792b5ed7988bf
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-12-02 12:01:34 +0900

    Fix:filmsコントローラのコードを修正 #11

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -1,7 +1,19 @@
-class Api::V1::FilmsController < ApplicationController
-  def search
-    films = GetFilmData.search_films(params[:search_word], params[:page])
-
-    render json: films, status: :ok
+module Api
+  module V1
+    class FilmsController < ApplicationController
+      def search
+        films = GetFilmData.search_films(params[:search_word], params[:page])
+    
+        render json: films, status: :ok
+      end
+    end
   end
 end
+
+# class Api::V1::FilmsController < ApplicationController
+#   def search
+#     films = GetFilmData.search_films(params[:search_word], params[:page])
+
+#     render json: films, status: :ok
+#   end
+# end

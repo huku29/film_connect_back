5bc08a3816f57d90ec2711dc507a322f9d8a95b1
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-12-28 11:09:29 +0900

    Add:ルーティング追加 #17

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/received_letters', to: 'received_letters#index'
+      get '/received_letters/info', to: 'received_letters#info'
+      post '/received_letters', to: 'received_letters#create'
+      get '/letters', to: 'letters#index'
       get 'letters/random', to: 'letters#random'
       post '/letters', to: 'letters#create'
       post '/users', to: 'users#create'
       get 'films/search'
+      get 'films/info'
     end
   end
 end

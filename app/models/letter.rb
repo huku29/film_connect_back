45a46139cdeeb4b98e6ad93d9d43e48eb47c4dbc
Nao Fukui <96228122+huku29@users.noreply.github.com>
2022-12-28 11:05:06 +0900

    Add:新たなアソシエーションを各モデルに定義 #17

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,5 +1,6 @@
 class Letter < ApplicationRecord
   belongs_to :user
+  has_many :received_letters, dependent: :destroy
 
   validates :recommend_point, presence: true, length: { minimum: 30 }
   validates :film_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :letters, dependent: :destroy
+  has_many :letters, through: :received_letters
+  has_many :received_letters, dependent: :destroy
 end

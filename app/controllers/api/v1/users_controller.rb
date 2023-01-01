6b886609e53556ea6fb8d7db85fae5aae709dcb4
Nao Fukui <96228122+huku29@users.noreply.github.com>
2023-01-01 13:09:07 +0900

    Add:Twitterのアカウント名を取得 #19

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,16 +9,22 @@ module Api
         create_user(@auth,params[:twitterUserName])
       end
 
-      # include UpdateUserConcern
-      # def update
-      #   update_user(@auth)
-      # end
+      def info
+        get_user_infos(params[:user_id])
+      end
+
 
       private
 
       def set_auth
         @auth = authenticate_token_by_firebase
       end
+
+      def get_user_infos(user_id)
+        user_info = User.where(id: user_id).all
+        render json: user_info, status: :ok
+      end
+
     end
   end
 end

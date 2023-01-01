0656bc8b51837c07f54c929cc8bbe6329e94b19f
Nao Fukui <96228122+huku29@users.noreply.github.com>
2023-01-01 13:31:35 +0900

    Add:受け取ったレターと自分が作成したレター以外を受け取り画面に表示 #21

--- app/controllers/concerns/api/v1/random_letter_concern.rb
+++ app/controllers/concerns/api/v1/random_letter_concern.rb
@@ -4,14 +4,12 @@ module Api
       extend ActiveSupport::Concern
 
       def random_letter(current_user)
-        current_user_id = current_user.id
-        received_letters = current_user.received_letters
-        received_letter_id_list = received_letters.pluck(:letter_id)
+        received_letter_id_list = current_user.received_letters.pluck(:letter_id)
         # 自分以外のユーザーが作成したレターの数を定義してる
-        other_user_create_letters = Letter.where.not(user_id: current_user_id)
-        count = other_user_create_letters.length
+        other_user_letters = Letter.where.not(user_id:  current_user.id)
+        count = other_user_letters.where.not(id: received_letter_id_list).length
         # offsetは歯ぬきをよしなに処理してくれる。自分が作っていないレターの総数から自分の作っていないレターをランダムに選択して、一つ表示させる。
-        other_user_create_letters.where.not(id: received_letter_id_list).offset(rand(count -1)).limit(1).first
+        other_user_letters.where.not(id: received_letter_id_list).offset(rand(count -1)).limit(1).first
       end
     end
   end

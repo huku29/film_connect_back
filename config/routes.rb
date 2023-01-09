bdd29c96865e4f0306f92087f7fb045b5cb33889
Nao Fukui <96228122+huku29@users.noreply.github.com>
2023-01-09 10:27:44 +0900

    Add:not_watch_film_lettersコントローラの作成 #23

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,17 @@ Rails.application.routes.draw do
       post '/received_letters', to: 'received_letters#create'
       get '/letters', to: 'letters#index'
       get 'letters/random', to: 'letters#random'
+      get '/letters/rank', to:'letters#rank'
       post '/letters', to: 'letters#create'
       get '/users/info', to: 'users#info'
       post '/users', to: 'users#create'
       get 'films/search'
       get 'films/info'
+      get '/not_watch_film_letters', to: 'not_watch_film_letters#index'
+      get '/not_watch_film_letters/rank', to: 'not_watch_film_letters#rank'
+      get '/not_watch_film_letters/info', to: 'not_watch_film_letters#info'
+      get '/not_watch_film_letters/not_watch_film_letters_detail', to: 'not_watch_film_letters#not_watch_film_letters_detail'
+      post '/not_watch_film_letters', to: 'not_watch_film_letters#create'
     end
   end
 end

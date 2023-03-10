f520d38393408a59bb7adae41b2c26d052c933d0
Nao Fukui <96228122+huku29@users.noreply.github.com>
2023-03-10 11:00:16 +0900

    Add:モデルのスキーマ情報を見やすくする

--- spec/factories/not_watch_film_letters.rb
+++ spec/factories/not_watch_film_letters.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: not_watch_film_letters
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  letter_id  :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_not_watch_film_letters_on_letter_id  (letter_id)
+#  index_not_watch_film_letters_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (letter_id => letters.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :not_watch_film_letter do
     user { nil }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  uid        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#  index_users_on_uid   (uid) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     name { "MyString" }
